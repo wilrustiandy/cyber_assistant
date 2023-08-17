0f83b80746bb7535406150b02294e517c2a51385
Wilson Rustiandy <wilrustiandy@gmail.com>
2023-08-17 23:26:12 +0700

    Initial project

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Cyber Assistant"
+include(":app")
+ 
